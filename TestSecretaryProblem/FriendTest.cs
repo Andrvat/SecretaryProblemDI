b9aaddbdeb840b51c54915b9914be5c74c27c6ed
Andrvat <andreivalitov2001@gmail.com>
2022-10-14 17:09:19 +0700

    Add unit tests for hall

--- TestSecretaryProblem/FriendTest.cs
+++ TestSecretaryProblem/FriendTest.cs
@@ -9,7 +9,7 @@ public class FriendTest
     public void UnknownContenderTest()
     {
         var friend = new Friend();
-        var (first, second) = GetTwoRandomContenders(50, 50);
+        var (first, second) = TestsHelper.GetTwoRandomContenders(50, 50);
         Assert.Throws<InvalidOperationException>(() => friend.ReplyToComparison(first, second));
     }
 
@@ -17,14 +17,8 @@ public class FriendTest
     public void CorrectComparisonTest()
     {
         var friend = new Friend();                                                                  
-        var (first, second) = GetTwoRandomContenders(50, 60);
+        var (first, second) = TestsHelper.GetTwoRandomContenders(50, 60);
         friend.NotifyAboutContender(first);
         Assert.True(friend.ReplyToComparison(second, first));
-    }
-
-    private static (RatingContender, RatingContender) GetTwoRandomContenders(int firstRating, int secondRating)
-    {
-        return (new RatingContender("C", "B", "A", firstRating),
-            new RatingContender("X", "Y", "Z", secondRating));
     }
 }
---
+++ TestSecretaryProblem/TestsHelper.cs
@@ -0,0 +1,12 @@
+﻿using SecretaryProblemDI;
+
+namespace TestSecretaryProblem;
+
+public class TestsHelper
+{
+    public static (RatingContender, RatingContender) GetTwoRandomContenders(int firstRating, int secondRating)
+    {
+        return (new RatingContender("C", "B", "A", firstRating),
+            new RatingContender("X", "Y", "Z", secondRating));
+    }
+}
