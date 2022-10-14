64f8628c019519c7f9d1a13d70619f045937e338
Andrvat <andreivalitov2001@gmail.com>
2022-10-14 16:34:26 +0700

    Add test for unique contender names

--- TestSecretaryProblem/ContenderGeneratorTest.cs
+++ TestSecretaryProblem/ContenderGeneratorTest.cs
@@ -1,11 +1,23 @@
-﻿namespace TestSecretaryProblem;
+﻿using SecretaryProblemDI;
+
+namespace TestSecretaryProblem;
 using Xunit;
 
 public class ContenderGeneratorTest
 {
     [Fact]
-    public void Test1()
+    public void ContendersUniqueSetTest()
     {
+        const int contendersNumber = 100;
+        var contenders = new ContendersGenerator().GetShuffledContenders();
+        Assert.Equal(contendersNumber, contenders.Count);
+
+        var contendersSet = new HashSet<RatingContender>();
+        foreach (var contender in contenders)
+        {
+            contendersSet.Add(contender);
+        }
         
+        Assert.Equal(contendersNumber, contendersSet.Count);
     }
 }
