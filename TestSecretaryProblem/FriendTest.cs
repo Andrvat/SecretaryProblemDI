2b3a30fa6a778a8e3e07921fd39b76ddfa5790cf
Andrvat <andreivalitov2001@gmail.com>
2022-10-14 16:50:37 +0700

    Add unit tests for friend

---
+++ TestSecretaryProblem/FriendTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic;
+using SecretaryProblemDI;
+
+namespace TestSecretaryProblem;
+
+public class FriendTest
+{
+    [Fact]
+    public void UnknownContenderTest()
+    {
+        var friend = new Friend();
+        var (first, second) = GetTwoRandomContenders(50, 50);
+        Assert.Throws<InvalidOperationException>(() => friend.ReplyToComparison(first, second));
+    }
+
+    [Fact]
+    public void CorrectComparisonTest()
+    {
+        var friend = new Friend();                                                                  
+        var (first, second) = GetTwoRandomContenders(50, 60);
+        friend.NotifyAboutContender(first);
+        Assert.True(friend.ReplyToComparison(second, first));
+    }
+
+    private static (RatingContender, RatingContender) GetTwoRandomContenders(int firstRating, int secondRating)
+    {
+        return (new RatingContender("C", "B", "A", firstRating),
+            new RatingContender("X", "Y", "Z", secondRating));
+    }
+}
